7f5f93af29cf9fbf4b8004a522bd163c63d4819a
Erangolan <37651948+Erangolan@users.noreply.github.com>
2021-08-30 13:44:08 +0300

    added eslint

--- app/src/router/router.js
+++ app/src/router/router.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import {BrowserRouter as Router,Route, Redirect,Switch} from 'react-router-dom';
 import Header from '../Components/Header';
@@ -8,18 +9,18 @@ import MyRecipes from '../Components/MyRecipes/MyRecipes';
 import App from '../Components/App'
 
 const ReactRouter = () => {
-    
-    return (
-        <React.Fragment>
-            
-            <Switch>
-                <Route exact path="/" component={Login}/>
-                <Route path="/MyRecipes" component={MyRecipes}/>
-                <Route path="/Form" component={Form}/>
-                <Route path="/Recipes" component={Recipes}/>
-            </Switch>
-        </React.Fragment>
-    )
+
+  return (
+    <React.Fragment>
+
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route path="/MyRecipes" component={MyRecipes}/>
+        <Route path="/Form" component={Form}/>
+        <Route path="/Recipes" component={Recipes}/>
+      </Switch>
+    </React.Fragment>
+  )
 }
 
 export default ReactRouter;
