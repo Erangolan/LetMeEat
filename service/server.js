fb6d834a8859b86f90753f191fb192a57a58200b
Erangolan <37651948+Erangolan@users.noreply.github.com>
2021-09-05 18:26:53 +0300

    bulding stracture

--- service/server.js
+++ service/server.js
@@ -26,49 +26,12 @@ app.use((req, res, next) => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const {
-  exampleDelivery,
-} = require('./src/lib')
-
 const recipes = require('./src/routes/recipes/index')
 const user = require('./src/routes/user/index')
+const registration = require('./src/routes/registration/index')
 
 app.use('/recipes', recipes)
 app.use('/user', user)
-
-const bootstrap = async () => {
-  console.log('start cron job...')
-  await exampleDelivery()
-}
-
-bootstrap()
+app.use('/registration', registration)
 
 app.listen(PORT, () => console.log('server listening on port: ', PORT))
-
-// const express = require('express')
-
-// const {
-//   exampleDelivery,
-// } = require('./src/lib')
-
-// const {
-//   PORT,
-// } = require('./src/consts')
-
-// const bootstrap = async (app) => {
-//   console.log('start cron job...')
-//   await exampleDelivery()
-
-//   if (!PORT) {
-//     console.log('missing port, unable to start express server')
-//     return process.exit(1)
-//   }
-
-//   return app.listen(PORT, () => {
-//     console.log(`server is up and running on ${PORT}`)
-//   })
-// }
-
-// const app = express()
-
-// bootstrap(app)
